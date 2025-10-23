16b140724ca192a4ca0b48679f24bf805fc2f95b
tavleen2 <tavleen251104@gmail.com>
2025-10-23 12:31:19 -0400

    Flask

--- flask/main.py
+++ flask/main.py
@@ -17,4 +17,9 @@ def contact():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
+
 app.run(debug=True)
