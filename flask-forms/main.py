30d15c98751b858e486f80403515f9ef0d2b6b1b
tavleen2 <tavleen251104@gmail.com>
2025-10-26 23:15:35 -0400

    Flask

--- flask-forms/main.py
+++ flask-forms/main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods= ["GET", "POST"])    #accepts both GET and POST request
+@app.route("/", methods= ["GET", "POST"])   
 def hello_world():
-    return "<p> Hello !!</p>"
+    print(request.method)
+    return render_template("contact.html")
 
 app.run(port= 8000, debug=True)
