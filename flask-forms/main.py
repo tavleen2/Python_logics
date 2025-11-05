cfe8ccb7ac95ee7167b9b32c3c322c6b7d160265
tavleen2 <tavleen251104@gmail.com>
2025-11-05 12:02:03 -0500

    Flask-forms

--- flask-forms/main.py
+++ flask-forms/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods= ["GET", "POST"])   
 def hello_world():
     if (request.method == "POST"):
-        
+        #handle the form
         with open("file.txt", "w") as f:
             f.write(f"The name is: {request.form['name']} , \n The email is : {request.form['email']}, \n The DOB is : {request.form['dob']}\n")
         return render_template("contact.html")
